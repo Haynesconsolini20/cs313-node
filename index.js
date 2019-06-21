70049ee48030ab28bdb3eeb62a1c75a71c763418
Sam Haynes <haynesconsolini@gmail.com>
2019-06-21 08:31:02 -0700

    finished assignment 09

--- index.js
+++ index.js
@@ -7,4 +7,11 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/rates', (req, res) => res.render('pages/rates'))
+  .get('/getRate', function(req, res){ 
+    console.log(req.query.weight);
+    console.log(req.query.mail);
+    res.locals.query = req.query;
+    res.render('pages/getRate')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
